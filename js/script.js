6743ed7a3f237f7f35ca984a2effa0be75e9806b
Francesco-Rianna <maradonino98@hotmail.it>
2024-03-19 16:29:38 +0100

    added reasoning before starting to write code

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
-alert('test')
+// Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi.
+// Dopo 30 secondi i numeri scompaiono e l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+// Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+// Consigli del giorno:
+// * Pensate prima in italiano.
+// * Dividete in piccoli problemi la consegna.
+// * Individuate gli elementi di cui avete bisogno per realizzare il programma.
+
+// creo un array vuto
+// faccio un ciclo for per 5 volte dove  pusho 5 numeri casuali tramite una funzione che mi genera numeri casuali
+// stampo in pagina i 5 numeri dell'array
+// faccio una funzione che mi fa scomparire dopo 30 secondi i numeri generati
+// chiedo all'utente quali erano i numeri in  pagina
+// confronto i numeri inseriti dall'utente con quelli che erano stampati in pagina
+// stampo il risultato sempre in pagina di quanti numeri sono stati indovinati
+
