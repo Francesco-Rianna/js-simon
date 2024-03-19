d3989b17a2fa3aad7562cb1f2da7baa46a27cf87
Francesco-Rianna <maradonino98@hotmail.it>
2024-03-19 17:16:04 +0100

    feat: added random numbers in array and print in document

--- js/script.js
+++ js/script.js
@@ -6,11 +6,30 @@
 // * Dividete in piccoli problemi la consegna.
 // * Individuate gli elementi di cui avete bisogno per realizzare il programma.
 
+// prendo l'elemento del dom dove inserire l'array di numeri
+const containerDom = document.querySelector('.text')
+console.log(containerDom)
 // creo un array vuto
+const arrayNumbersCpu = [];
+console.log(arrayNumbersCpu)
 // faccio un ciclo for per 5 volte dove  pusho 5 numeri casuali tramite una funzione che mi genera numeri casuali
+for (let i = 0; i < 5; i++) {
+    arrayNumbersCpu.push(getRndInteger(1, 100))
+
+}
 // stampo in pagina i 5 numeri dell'array
+containerDom.innerHTML += arrayNumbersCpu
+
+
+
+
+
 // faccio una funzione che mi fa scomparire dopo 30 secondi i numeri generati
 // chiedo all'utente quali erano i numeri in  pagina
 // confronto i numeri inseriti dall'utente con quelli che erano stampati in pagina
 // stampo il risultato sempre in pagina di quanti numeri sono stati indovinati
 
+// FUNCTIONS
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
